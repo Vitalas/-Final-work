443992484c6f37aa9482d4991442243c0143460c
Vitalas <Vitalas_2004@mail.ru>
2023-11-21 21:09:38 +0300

    Написал ввод первого массива с консоли

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintString(string[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int numberOfString = Convert.ToInt32(Console.ReadLine());
+string[] firstArray = new string[numberOfString];
+string el = "";
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    Console.Write($"Введите символы {i+1}й строки: ");
+    el = Console.ReadLine();
+    firstArray[i] = el;
+}
+
+PrintString(firstArray);
