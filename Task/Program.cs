78749f17274d96c3a9820c0d87e8929afdf54013
Vitalas <Vitalas_2004@mail.ru>
2023-11-24 19:10:48 +0300

    Отредактировал ввод/вывод

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,31 +1,34 @@
 ﻿void PrintString(string[] arr)
 {
-    Console.Write("[ ");
-    for (int i = 0; i < arr.Length; i++)
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
     {
-        Console.Write($"{arr[i]} ");
+        Console.Write($"\"{arr[i]}\",");
     }
-    Console.WriteLine("]");
+    Console.WriteLine($"\"{arr[^1]}\"]");
 }
 
-Console.Write("Введите количество строк в массиве: ");
+Console.Write("Введите количество строк в первом массиве 1-5(включительно): ");
 int numberOfString = Convert.ToInt32(Console.ReadLine());
+
+if ((numberOfString > 5) || (numberOfString < 1))
+{
+    Console.WriteLine("Не корректный ввод!");
+    return;
+}
 string[] firstArray = new string[numberOfString];
 
 for (int i = 0; i < firstArray.Length; i++)
 {
-    Console.Write($"Введите символы {i+1}й строки: ");
+    Console.Write($"Введите символы {i + 1}й строки: ");
     firstArray[i] = Console.ReadLine();
 }
-
-PrintString(firstArray);
 
 int numberOfString2 = 0;
 for (int i = 0; i < firstArray.Length; i++)
 {
     if (firstArray[i].Length <= 3) numberOfString2++;
 }
-
 string[] secondArray = new string[numberOfString2];
 
 int j = 0;
@@ -37,4 +40,22 @@ for (int i = 0; i < firstArray.Length; i++)
         j++;
     }
 }
-PrintString(secondArray);
+
+if (numberOfString2 == 0)
+{
+    Console.WriteLine();
+    Console.Write("Первый массив: ");
+    PrintString(firstArray);
+    Console.Write("Массив из строк первого массива, длинна которых меньше или ровна трём символам: ");
+    Console.WriteLine("[]");
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine();
+    Console.Write("Первый массив: ");
+    PrintString(firstArray);
+    Console.Write("Массив из строк первого массива, длинна которых меньше или ровна трём  символам: ");
+    PrintString(secondArray);
+    Console.WriteLine();
+}
